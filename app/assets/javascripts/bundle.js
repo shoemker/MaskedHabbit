a886c6624f6284c2d6fa5671de54f746da84977f
shoemker <acubed75@gmail.com>
2022-03-28 08:54:04 -0700

    changed map text color to black

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -1752,10 +1752,8 @@ var Map = function Map(_ref) {
     className: "map"
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
     className: "google-map"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(google_map_react__WEBPACK_IMPORTED_MODULE_1__["default"], {
-    bootstrapURLKeys: {
-      key: window.googleAPIKey
-    },
+  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(google_map_react__WEBPACK_IMPORTED_MODULE_1__["default"] // bootstrapURLKeys={{ key: window.googleAPIKey}}
+  , {
     defaultCenter: location,
     defaultZoom: zoomLevel
   }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_locationPin__WEBPACK_IMPORTED_MODULE_2__["default"], {
